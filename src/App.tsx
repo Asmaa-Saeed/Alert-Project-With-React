903ac1d7681be29dba8bdd0d69bed8a8df7cd2cc
Asmaa-Saeed <asmaasaeed2082003@gmail.com>
2024-12-04 20:44:25 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App = () => {
   
   return (
     <>
+        
       <Alert type={"info-wrapper"} icon={<Info size={18}  />} title='info' desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
       <Alert type={"success-wrapper"} icon={<PrinterCheck size={18}/>} title={'Success'} desc={ <>"Lorem Ipsum is simply. Lorem Ipsum has been the industry's standard dummy text ever , when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."</>} />
       <Alert type={"warning-wrapper"} icon={<TriangleAlert size={18}  />} title='warning' desc="Lorem Ipsum is simply. Lorem Ipsum has been the industry's standard , when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." />
